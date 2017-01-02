27a7d1e87184a445c33faee3919259f91ad6b125
naoyashiga <naoyashiga0@gmail.com>
2017-01-02 15:32:19 +0900

    modify task name

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('server', function() {
      gulp.watch(['src/js/**/*.js','src/index.html']); 
 });
 
-gulp.task('gh-pages', () => {
+gulp.task('deploy', () => {
   return gulp.src('./src/**/*')
     .pipe(ghPages({
       remoteUrl: "https://github.com/naoyashiga/my-dying-message"
