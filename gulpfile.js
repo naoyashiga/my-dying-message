89f374d8286b59b11a9e8519f243eebcde789079
naoyashiga <naoyashiga0@gmail.com>
2017-01-02 15:34:00 +0900

    Update 2017-01-02T06:33:49.565Z

--- gulpfile.js
+++
@@ -1,25 +0,0 @@
-var gulp = require('gulp'), 
-    ghPages = require('gulp-gh-pages'), 
-    webserver = require('gulp-webserver');
-
-gulp.task('server', function() {
-  gulp.src('./src/')
-    .pipe(webserver({
-      livereload: true,
-      open: true
-    }));
-});
-
-// Rerun the task when a file changes
- gulp.task('watch', function() {
-     gulp.watch(['src/js/**/*.js','src/index.html']); 
-});
-
-gulp.task('deploy', () => {
-  return gulp.src('./src/**/*')
-    .pipe(ghPages({
-      remoteUrl: "https://github.com/naoyashiga/my-dying-message"
-    }));
-});
-
-  gulp.task('default', ['watch','server']);
